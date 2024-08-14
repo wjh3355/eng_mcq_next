5ac64a22f68650906901e843b0d52c2250e803fa
jhw3355 <jhwang0324@gmail.com>
2024-08-14 13:53:57 +0800

    mongodb 5

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -4,25 +4,25 @@ const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
-let cachedClient = null;
-let cachedDb = null;
+// let cachedClient = null;
+// let cachedDb = null;
 
 export async function connectToDB() {
-   if (cachedClient && cachedDb) {
-      return { db: cachedDb, client: cachedClient };
-   }
+   // if (cachedClient && cachedDb) {
+   //    return { db: cachedDb, client: cachedClient };
+   // }
   
    try {
       await client.connect();
 
       const qnDB = client.db("english_questions");
 
-      cachedClient = client;
-      cachedDb = db;
+      // cachedClient = client;
+      // cachedDb = qnDB;
 
       return { qnDB, client };
-   } catch (err) {
+   } catch (error) {
       console.error("Failed to connect to MongoDB", error);
-      throw error;
+      throw new Error("Database connection failed");
    }
 }
