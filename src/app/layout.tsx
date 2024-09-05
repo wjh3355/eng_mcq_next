34da6f4543c3879ca9c235fc2a32ec087ddd8ac6
jhw3355 <jhwang0324@gmail.com>
2024-09-05 12:34:07 +0800

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,33 @@
-import MainNavbar from "@/app/ui/MainNavbar";
-import { inter } from "@/lib/fonts";
-import "@/styles/global.css";
 import React from "react";
+
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import "@/styles/global.css";
+import { inter } from "@/lib/fonts";
+
+import MainNavbar from "@/app/ui/MainNavbar";
 
 export const metadata = {
    title: "Sunbird English",
-   description: "A side project",
+   description: "Simple english vocabulary MCQ quiz app. For Singaporean students.",
    icons: {
       icon: "/favicon.ico",
    },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ 
+   children 
+}: { 
+   children: React.ReactNode 
+}) {
    return (
       <html lang="en">
          <body className={`${inter.className} antialiased`}>
             <MainNavbar />
             {children}
             <Analytics />
+            <SpeedInsights />
          </body>
       </html>
    );
