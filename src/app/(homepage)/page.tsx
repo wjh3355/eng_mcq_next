55122997aef384aa47a180a61c016288fab312ee
jhw3355 <jhwang0324@gmail.com>
2024-09-12 21:56:45 +0800

    changed notice format

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,25 +1,47 @@
-import Notice from "./Notice";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Link from "next/link";
+import { connectToDB } from "@/lib/connectToDB";
 
-export default function Page() {
+export default async function Page() {
+
+   let noticeHtml: string = "";
+
+   try {
+      const { db } = await connectToDB("notices");
+      const data = await db
+         .collection("notice")
+         .findOne({}, { projection: { _id: 0, html: 1 } });
+
+      noticeHtml = data?.html || "";
+   } catch (error) {
+      console.error("Could not fetch notice for homepage:", error);
+   }
+
    return (
       <Container>
-         <Row className="my-3">
+         <Row className="mt-3">
             <Col className="text-center">
                <h4>Welcome to Sunbird English</h4>
             </Col>
          </Row>
 
-         <Row>
+         <Row className="mt-3">
             <Col>
-               <Notice/>
+               <div className="card">
+                  <div className="card-header">Notice</div>
+                  <div className="card-body">
+                     <div
+                        className="card-text"
+                        dangerouslySetInnerHTML={{__html: noticeHtml}}
+                     />
+                  </div>
+               </div>
             </Col>
          </Row>
 
-         <Row>
+         <Row className="mt-3">
             <Col className="d-flex justify-content-center">
                <Link href="/demo" className="btn btn-lg btn-primary">
                   Demo Questions
