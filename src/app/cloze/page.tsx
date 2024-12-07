4a24ff87ab3eff8804fe6b6f7ff8155d005549e8
Wang Jiahua <169117299+wjh3355@users.noreply.github.com>
2024-12-07 22:02:43 +0700

    Update page.tsx

--- src/app/cloze/page.tsx
+++ src/app/cloze/page.tsx
@@ -1,6 +1,8 @@
 import checkNormalUserAuth from "@/serverFuncs/checkNormalUserAuth";
 import ClozeApp from "./ClozeApp";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
    await checkNormalUserAuth();
 
