94b13becab37c58ca2086e88e093a037086bcb0f
jhw3355 <jhwang0324@gmail.com>
2024-12-17 21:19:53 +0800

    fk

--- src/app/cloze/page.tsx
+++ src/app/cloze/page.tsx
@@ -5,6 +5,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
 
    await checkNormalUserAuth();
