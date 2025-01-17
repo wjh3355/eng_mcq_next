cf595e0b79abc9133fc0fff7be16d5e8e2745b3c
jhw3355 <jhwang0324@gmail.com>
2025-01-17 18:19:07 +0800

    fixed bug

--- src/components/SuggestionsForm.tsx
+++ src/components/SuggestionsForm.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { useFormik } from "formik";
-import Button from "react-bootstrap/button";
+// import React from "react";
+// import { useFormik } from "formik";
+// import Button from "react-bootstrap/button";
 
-export default function SuggestionsForm() {
+// export default function SuggestionsForm() {
 
-   const formik = useFormik({
-      initialValues: {
-         text: "",
-      },
-      onSubmit: async (values) => {
+//    const formik = useFormik({
+//       initialValues: {
+//          text: "",
+//       },
+//       onSubmit: async (values) => {
          
-         alert(JSON.stringify(values));
-      },
-   });
+//          alert(JSON.stringify(values));
+//       },
+//    });
 
-   return (
-      <form onSubmit={formik.handleSubmit} className="mt-3">
-         <p>If you have any suggestions for this website, please let us know:</p>
-         <textarea
-            id="text"
-            name="text"
-            onChange={formik.handleChange}
-            value={formik.values.text}
-            style={{ resize: "both" }}
-         />
-         <br/>
-         <Button type="submit" variant="primary">Submit</Button>
-      </form>
-   );
-};
+//    return (
+//       <form onSubmit={formik.handleSubmit} className="mt-3">
+//          <p>If you have any suggestions for this website, please let us know:</p>
+//          <textarea
+//             id="text"
+//             name="text"
+//             onChange={formik.handleChange}
+//             value={formik.values.text}
+//             style={{ resize: "both" }}
+//          />
+//          <br/>
+//          <Button type="submit" variant="primary">Submit</Button>
+//       </form>
+//    );
+// };
